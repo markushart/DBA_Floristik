54d67ac3048dc0dacd9b89d4858d9570650a85ea
Sascha Nickel <56337965+sumosascha@users.noreply.github.com>
2022-06-20 17:37:24 +0200

    Benutername validation fertig

---
+++ src/main/java/org.validator/PasswordValidator.java
@@ -0,0 +1,36 @@
+/*
+ * April 2022
+ */
+package org.validator;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+@FacesValidator(value="pwdValidator")
+public class PasswordValidator implements Validator{
+
+    @Override
+    public void validate(FacesContext fc, UIComponent uic, Object obj) 
+            throws ValidatorException {
+        
+        String pwd = obj.toString();
+        FacesMessage fm;
+        
+        if(pwd.length()>=5)
+        {
+           fm = new FacesMessage("Login-Name formal korrekt!");
+           FacesContext.getCurrentInstance().addMessage(uic.getClientId(), fm);
+        }
+        else
+        {
+            fm = new FacesMessage("Hinweis: min. 5 Zeichen!");
+            throw new ValidatorException(fm);
+        }
+        
+    }
+
+}
