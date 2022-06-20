df19fb986f72c0ce228965e0b322438f5a7bf0fa
Sascha Nickel <56337965+sumosascha@users.noreply.github.com>
2022-06-20 17:44:44 +0200

    des Passwortes (mit einer Validator-Klasse)!

--- src/main/java/org.validator/PasswordValidator.java
+++ src/main/java/org.validator/PasswordValidator.java
@@ -12,17 +12,17 @@ import javax.faces.validator.ValidatorException;
 
 @FacesValidator(value="pwdValidator")
 public class PasswordValidator implements Validator{
-
+    
     @Override
     public void validate(FacesContext fc, UIComponent uic, Object obj) 
             throws ValidatorException {
-        
+        System.out.println("pwdValidator aufgerufen");
         String pwd = obj.toString();
         FacesMessage fm;
         
         if(pwd.length()>=5)
         {
-           fm = new FacesMessage("Login-Name formal korrekt!");
+           fm = new FacesMessage("Passwort formal korrekt!");
            FacesContext.getCurrentInstance().addMessage(uic.getClientId(), fm);
         }
         else
